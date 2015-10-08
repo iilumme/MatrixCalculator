12d20f22a135c6761acf67f67d40ab0311bba640
iilumme <iilumme@cs.helsinki.fi>
2015-10-08 22:50:24 +0300

    :)

--- MatrixCalculator/src/main/java/matrixcalculator/logic/InverseCalculator.java
+++ MatrixCalculator/src/main/java/matrixcalculator/logic/InverseCalculator.java
@@ -30,98 +30,98 @@ public class InverseCalculator {
 
         } else {
 
-            int rows = a.getRows();
-            int columns = a.getColumns();
-
-            Matrix lowerTriangularMatrix = new Matrix(rows, columns);
-            Matrix upperTriangularMatrix = new Matrix(rows, columns);
-
-            lowerTriangularMatrix.setNumbers(setLowerNumbers(rows, columns));
-            upperTriangularMatrix.setNumbers(setUpperNumbers(rows, columns));
-
-            double[][] numbers = a.getNumbers();
-
-            //rows
-            for (int i = 0; i < rows; i++) {
-                //columns
-                for (int j = 0; j < columns; j++) {
-                    double elements[] = new double[rows * 2];
-                    int pos = 0;
-                    //The position of the 'X', the number which should be discovered.
-                    int[][] posOfX = new int[1][2];
-                    //To which matrix the 'X' belongs.
-                    String whichMatrix = null;
-
-                    //each number in the column
-                    for (int k = 0; k < rows; k++) {
-                        elements[pos] = lowerTriangularMatrix.getNumbers()[i][k];
-                        pos++;
-                        elements[pos] = upperTriangularMatrix.getNumbers()[k][j];
-                        pos++;
-
-                        if (lowerTriangularMatrix.getNumbers()[i][k] == Double.MAX_VALUE && upperTriangularMatrix.getNumbers()[k][j] != 0) {
-                            posOfX[0][0] = i;
-                            posOfX[0][1] = k;
-                            whichMatrix = "L";
-                        } else if (lowerTriangularMatrix.getNumbers()[i][k] != 0 && upperTriangularMatrix.getNumbers()[k][j] == Double.MAX_VALUE) {
-                            posOfX[0][0] = i;
-                            posOfX[0][1] = j;
-                            whichMatrix = "U";
-                        }
-                    }
-
-                    //The elements which are used in the calculation of 'X'.
-                    double[] countables = new double[2];
-                    //The position where the elements is to be added.
-                    int posit = 0;
-
-                    for (int k = 0; k < elements.length; k++) {
-                        double first = elements[k];
-                        double second = elements[k + 1];
-
-                        if (first == 1 && second == Double.MAX_VALUE) {
-                            countables[posit] = second;
-                            posit++;
-                        } else if (first == Double.MAX_VALUE && second == 1) {
-                            countables[posit] = first;
-                            posit++;
-                        } else if (first != 0 && second == Double.MAX_VALUE) {
-                            countables[posit] = first;
-                            posit++;
-                            countables[posit] = second;
-                            posit++;
-                        } else if (first == Double.MAX_VALUE && second != 0) {
-                            countables[posit] = second;
-                            posit++;
-                            countables[posit] = first;
-                            posit++;
-                        } else if (first != 0 && second != 0) {
-                            double result = first * second;
-                            numbers[i][j] -= result;
-                        }
-
-                        k++;
-                    }
-
-                    if (countables[1] == 0) {
-                        double importantNumber = numbers[i][j];
-                        if (whichMatrix.equals("L")) {
-                            lowerTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
-                        } else {
-                            upperTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
-                        }
-                    } else {
-                        double importantNumber = numbers[i][j] / countables[0];
-                        if (whichMatrix.equals("L")) {
-                            lowerTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
-                        } else {
-                            upperTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
-                        }
-                    }
-                }
-            }
-            
-            IdentityMatrix identityMatrix = new IdentityMatrix(rows);
+//            int rows = a.getRows();
+//            int columns = a.getColumns();
+//
+//            Matrix lowerTriangularMatrix = new Matrix(rows, columns);
+//            Matrix upperTriangularMatrix = new Matrix(rows, columns);
+//
+//            lowerTriangularMatrix.setNumbers(setLowerNumbers(rows, columns));
+//            upperTriangularMatrix.setNumbers(setUpperNumbers(rows, columns));
+//
+//            double[][] numbers = a.getNumbers();
+//
+//            //rows
+//            for (int i = 0; i < rows; i++) {
+//                //columns
+//                for (int j = 0; j < columns; j++) {
+//                    double elements[] = new double[rows * 2];
+//                    int pos = 0;
+//                    //The position of the 'X', the number which should be discovered.
+//                    int[][] posOfX = new int[1][2];
+//                    //To which matrix the 'X' belongs.
+//                    String whichMatrix = null;
+//
+//                    //each number in the column
+//                    for (int k = 0; k < rows; k++) {
+//                        elements[pos] = lowerTriangularMatrix.getNumbers()[i][k];
+//                        pos++;
+//                        elements[pos] = upperTriangularMatrix.getNumbers()[k][j];
+//                        pos++;
+//
+//                        if (lowerTriangularMatrix.getNumbers()[i][k] == Double.MAX_VALUE && upperTriangularMatrix.getNumbers()[k][j] != 0) {
+//                            posOfX[0][0] = i;
+//                            posOfX[0][1] = k;
+//                            whichMatrix = "L";
+//                        } else if (lowerTriangularMatrix.getNumbers()[i][k] != 0 && upperTriangularMatrix.getNumbers()[k][j] == Double.MAX_VALUE) {
+//                            posOfX[0][0] = i;
+//                            posOfX[0][1] = j;
+//                            whichMatrix = "U";
+//                        }
+//                    }
+//
+//                    //The elements which are used in the calculation of 'X'.
+//                    double[] countables = new double[2];
+//                    //The position where the elements is to be added.
+//                    int posit = 0;
+//
+//                    for (int k = 0; k < elements.length; k++) {
+//                        double first = elements[k];
+//                        double second = elements[k + 1];
+//
+//                        if (first == 1 && second == Double.MAX_VALUE) {
+//                            countables[posit] = second;
+//                            posit++;
+//                        } else if (first == Double.MAX_VALUE && second == 1) {
+//                            countables[posit] = first;
+//                            posit++;
+//                        } else if (first != 0 && second == Double.MAX_VALUE) {
+//                            countables[posit] = first;
+//                            posit++;
+//                            countables[posit] = second;
+//                            posit++;
+//                        } else if (first == Double.MAX_VALUE && second != 0) {
+//                            countables[posit] = second;
+//                            posit++;
+//                            countables[posit] = first;
+//                            posit++;
+//                        } else if (first != 0 && second != 0) {
+//                            double result = first * second;
+//                            numbers[i][j] -= result;
+//                        }
+//
+//                        k++;
+//                    }
+//
+//                    if (countables[1] == 0) {
+//                        double importantNumber = numbers[i][j];
+//                        if (whichMatrix.equals("L")) {
+//                            lowerTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
+//                        } else {
+//                            upperTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
+//                        }
+//                    } else {
+//                        double importantNumber = numbers[i][j] / countables[0];
+//                        if (whichMatrix.equals("L")) {
+//                            lowerTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
+//                        } else {
+//                            upperTriangularMatrix.getNumbers()[posOfX[0][0]][posOfX[0][1]] = importantNumber;
+//                        }
+//                    }
+//                }
+//            }
+//            
+//            IdentityMatrix identityMatrix = new IdentityMatrix(rows);
             
             
             
